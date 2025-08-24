f0ba6041cdd33529de00c33a07cbf2ec4bec1f9b
PhamQuyNam <phamquynam2004@gmail.com>
2025-08-24 09:47:59 +0700

    commit4

--- account-service/src/main/java/com/npq/account_service/controller/AccountController.java
+++ account-service/src/main/java/com/npq/account_service/controller/AccountController.java
@@ -6,6 +6,7 @@ import com.npq.account_service.service.IAccountService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +25,11 @@ public class AccountController {
                 accounts,
                 new TypeToken<List<AccountDTO>>() {}.getType());
         return listAccountDTO;
+    }
+
+    @GetMapping("hello")
+    public String hello(){
+        return "hello";
     }
 
 }
