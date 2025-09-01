b09316ac89e1c2ca36b7321efd74b4a829b7656e
PhamQuyNam <phamquynam2004@gmail.com>
2025-09-01 10:24:19 +0700

    Commit 1/9

--- account-service/src/main/java/com/npq/account_service/controller/AccountController.java
+++ account-service/src/main/java/com/npq/account_service/controller/AccountController.java
@@ -5,19 +5,18 @@ import com.npq.account_service.dto.DepartmentDTO;
 import com.npq.account_service.entity.Account;
 import com.npq.account_service.feignclient.DepartmentFeignClient;
 import com.npq.account_service.service.IAccountService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
-<<<<<<< HEAD
+
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-=======
-import org.springframework.web.bind.annotation.GetMapping;
->>>>>>> f0ba6041cdd33529de00c33a07cbf2ec4bec1f9b
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -46,26 +45,25 @@ public class AccountController {
         return listAccountDTO;
     }
 
-<<<<<<< HEAD
-    @GetMapping("/hello")
-    public String hello(){
-        return greetingText;
-    }
-
+    @CircuitBreaker(name = "departmentService", fallbackMethod = "fallbackNotCallDepartmentService")
     @GetMapping("/{id}")
     public DepartmentDTO getDepartmentById(@PathVariable final int id){
         Account account = acService.findAccountById(id);
         int departmentId = account.getDepartment().getId();
-        //DepartmentDTO departmentDTO = restTemplate.getForObject("http://localhost:8080/api/v1/departments/"+departmentId, DepartmentDTO.class);
-        DepartmentDTO departmentDTO = departmentFeignClient.getDepartmentById(id);
+        DepartmentDTO departmentDTO = restTemplate.getForObject("http://localhost:8080/api/v1/departments/"+departmentId, DepartmentDTO.class);
+        //DepartmentDTO departmentDTO = departmentFeignClient.getDepartmentById(id);
         log.info("Department DTO: {}",departmentDTO);
         return departmentDTO;
     }
-=======
+
+    public String fallbackNotCallDepartmentService(int id, Throwable throwable){
+        return "Department Service Down";
+    }
+
     @GetMapping("hello")
     public String hello(){
         return "hello";
     }
 
->>>>>>> f0ba6041cdd33529de00c33a07cbf2ec4bec1f9b
+
 }
